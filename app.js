a307ab1a34f6f2b9e912a656d29803bba287f91b
zhangqiang0627 <johnny.q.zhang@outlook.com>
2018-10-19 23:27:31 +0800

    修改路径处理，支持跨平台路径处理。

--- app.js
+++ app.js
@@ -5,10 +5,11 @@ var apiConfig = require('./config/apiConfig');
 var dateUtils = require('./common/dateUtils');
 var serviceInvokeUtils = require('./common/serviceInvokeUtils');
 
-var second = sysConfig.scheduleIntervalMinute * 60 * 1000;
+var second = sysConfig.scheduleIntervalSeconds * 1000;
 
 function processLog() {
-  var logDir = sysConfig.logDir;
+  console.log('Processing...');
+  var logDir = path.join(path.resolve(__dirname, '..'), sysConfig.logDir);
   var host = apiConfig.StoreService.host;
   var port = apiConfig.StoreService.port;
   var systemLogPath = apiConfig.StoreService.path.systemLog;
@@ -16,24 +17,33 @@ function processLog() {
 
   //遍历每一个文件
   fileList.forEach(function (fileName) {
-    var filePath = path.join(sysConfig.logDir, fileName);
-    var fileContent = fs.readFileSync(filePath, "utf-8");//取得当前文件的内容
-    var logObj = JSON.parse(fileContent); //文件文件内容（json格式）转成js对象
+    try{
+      var filePath = path.join(logDir, fileName);
+      var statInfo = fs.statSync(filePath);
+      if(statInfo.isFile() && path.extname(fileName) === '.json'){
+        // var filePath = path.join(sysConfig.logDir, fileName);
+        var fileContent = fs.readFileSync(filePath, "utf-8");//取得当前文件的内容
+        var logObj = JSON.parse(fileContent); //文件文件内容（json格式）转成js对象
 
-    //调用API Service将日志信息保存到数据库中
-    serviceInvokeUtils.post(logObj, host, port, systemLogPath, function (result) {
-      printProcessLog('==='); //打印分割线
-      printProcessLog('processing log: ' + fileName); //打印处理日志
-      if(result.err){
-        printProcessLog('save data base failed: ' + fileName);
-        sendSMS(logObj, fileName, false); //发送短信
-        moveFile(fileName, false); //移动文件
-      }else{
-        printProcessLog('save data base success: ' + fileName);
-        sendSMS(logObj, fileName, true); //发送短信
-        moveFile(fileName, true); //移动文件
+        //调用API Service将日志信息保存到数据库中
+        serviceInvokeUtils.post(logObj, host, port, systemLogPath, function (result) {
+          printProcessLog('==='); //打印分割线
+          printProcessLog('processing log: ' + fileName); //打印处理日志
+          if(result.err){
+            printProcessLog('save data base failed: ' + fileName);
+            sendSMS(logObj, fileName, false); //发送短信
+            moveFile(fileName, false); //移动文件
+          }else{
+            printProcessLog('save data base success: ' + fileName);
+            sendSMS(logObj, fileName, true); //发送短信
+            moveFile(fileName, true); //移动文件
+          }
+        });
       }
-    });
+    }catch(err){
+      printProcessLog('processing file error: \n ' + fileName + '\n' + err.message ); //打印处理日志
+    }
+
   });
 }
 
@@ -43,29 +53,33 @@ function sendSMS(logObj, fileName, saveResult) {
 }
 
 function moveFile(fileName, saveResult) {
-  var sourceFile = path.join(sysConfig.logDir, fileName);
-  var destPath = saveResult ? path.join(sysConfig.processedDir, fileName) : path.join(sysConfig.errorDir, fileName);
+  var sourceFile = path.join(path.resolve(__dirname, '..'), sysConfig.logDir, fileName);
+  var processedDir = path.join(path.resolve(__dirname, '..'), sysConfig.processedDir);
+  var errorDir = path.join(path.resolve(__dirname, '..'), sysConfig.errorDir);
+  var destPath = saveResult ? path.join(processedDir, fileName) : path.join(errorDir, fileName);
 
   printProcessLog('move file ' + fileName + ' to ' + destPath);
 
-  if (!fs.existsSync(sysConfig.processedDir)) {
-    fs.mkdirSync(sysConfig.processedDir);
+  if (!fs.existsSync(processedDir)) {
+    fs.mkdirSync(processedDir);
   }
 
-  if (!fs.existsSync(sysConfig.errorDir)) {
-    fs.mkdirSync(sysConfig.errorDir);
+  if (!fs.existsSync(errorDir)) {
+    fs.mkdirSync(errorDir);
   }
 
   fs.rename(sourceFile, destPath, function (err) {
     if (err){
-      printProcessLog('move file [' + fileName + '] to ' + destPath + 'failed.');
+      printProcessLog('move file [' + fileName + '] to ' + destPath + ' failed.');
     }
   });
 }
 
 function printProcessLog(msg) {
   var logFileName = dateUtils.getCurrentDate() + '.log';
-  var logFilePath = path.join(sysConfig.processingDir, logFileName);
+  var processingDir = path.join(path.resolve(__dirname, '..'), sysConfig.processingDir);
+
+  var logFilePath = path.join(processingDir, logFileName);
   var logContent = '';
   if(msg.indexOf('===') >= 0){
     logContent = '=========================================================';
@@ -73,8 +87,8 @@ function printProcessLog(msg) {
     logContent = '[' + dateUtils.getCurrentDateTime() + ']' + ' ' + msg;
   }
 
-  if (!fs.existsSync(sysConfig.processingDir)) {
-    fs.mkdirSync(sysConfig.processingDir);
+  if (!fs.existsSync(processingDir)) {
+    fs.mkdirSync(processingDir);
   }
 
   console.log(logContent);
@@ -82,4 +96,6 @@ function printProcessLog(msg) {
   fs.appendFileSync(logFilePath, logContent + '\r\n');
 }
 
+console.log('Start...');
 setInterval(processLog, second);
+// processLog();
